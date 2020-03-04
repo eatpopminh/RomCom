8859c1b830f3d62c4cfe00bf1b9b47b978a59df8
eatpopminh <54875208+eatpopminh@users.noreply.github.com>
2020-03-04 15:19:57 -0800

    Testing some stuff

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		List<String> lines = Files.readAllLines(Paths.get("input.txt"));
+		int fuckUpDay = 20;
+		boolean flag = true;
+		int numOfDays = Integer.parseInt(lines.get(0));
+		int numOfProbes = Integer.parseInt(lines.get(1));
+		while(flag)
+		{	
+			numOfDays/2
+			if(numOfProbes==1)
+			{
+				break;
+			}
+		}
 
 	}
 
