361c8fc012db66221f05e4b43e04cb8c4860153d
eatpopminh <54875208+eatpopminh@users.noreply.github.com>
2020-03-06 15:23:24 -0800

    Testing priorityQueue. It's basically a heap tree.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Main {
 
@@ -12,15 +13,19 @@ public class Main {
 		boolean flag = true;
 		int numOfDays = Integer.parseInt(lines.get(0));
 		int numOfProbes = Integer.parseInt(lines.get(1));
-//		while(flag)
-//		{	
-//			
-//			if(numOfProbes==1)
-//			{
-//				break;
-//			}
-//		}
-
+		
+		
+		PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+		for(int i = 1 ; i<=216 ; i++)
+		{
+			minHeap.add(i);
+		}
+		
+		System.out.println(minHeap.size());
+		System.out.println(height(minHeap.size()));
 	}
-
+	static int height(int N) 
+    { 
+        return (int)Math.ceil(Math.log(N + 1) / Math.log(2)) - 1; 
+    } 
 }
