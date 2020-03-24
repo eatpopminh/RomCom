75156d606e78acd5675fcd8e0c181a41d97b6229
eatpopminh <54875208+eatpopminh@users.noreply.github.com>
2020-03-23 20:04:13 -0700

    cleaning

--- src/Main.java
+++ src/Main.java
@@ -18,18 +18,18 @@ public class Main {
     static int probing(int n, int k) 
     { 
         //2D array to save all data used, so we don't have to recalculate.
-        int attempts[][] = new int[n+1][k+1]; 
+        int array[][] = new int[n+1][k+1]; 
             
         //Base Case: if floor is 1 = 1 attempt. 
         for (int i = 1; i <= n; i++) 
         { 
-        	attempts[i][1] = 1;      
+        	array[i][1] = 1;      
         } 
            
         //Base Case: if given only 1 egg = have to loop all the floor.
         for (int i = 1; i <= k; i++) 
         {
-        	attempts[1][i] = i; 
+        	array[1][i] = i; 
         }  
         
         //Recursion to fill up the 2D array.
@@ -40,19 +40,19 @@ public class Main {
             for (int j = 2; j <= k; j++) 
             { 
             	//Max_VALUE is just a place holder.
-            	attempts[i][j] = Integer.MAX_VALUE; 
+            	array[i][j] = Integer.MAX_VALUE; 
                 for (int x = 1; x <= j; x++) 
                 { 
-                     int res = 1 + Math.max(attempts[i-1][x-1], attempts[i][j-x]); 
+                     int res = 1 + Math.max(array[i-1][x-1], array[i][j-x]); 
                      //Finding the worst case.
-                     if (res < attempts[i][j]) 
-                    	 attempts[i][j] = res; 
+                     if (res < array[i][j]) 
+                    	 array[i][j] = res; 
                 } 
             } 
         } 
            
         //Answer is at the end of the table.
-        return attempts[n][k]; 
+        return array[n][k]; 
   
     }
 
